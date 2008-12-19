0be3c4cd0a1dd97598677bbb4f6ccd8fc8ccb51b
John Boxall <john@handimobility.ca>
2008-12-19 09:25:31 -0800

    fixed some paths

---
+++ js/iphone.py
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>iPhone Scroll Demo</title>
+	    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js"></script>
+    	<script type="text/javascript" src="/js/iphone.js"></script>
+	</head>
+	<body>
+	
+	
+	
+	
+	<iframe id="">
+	
+	</iframe>
+	
+	</body>
+</html>
